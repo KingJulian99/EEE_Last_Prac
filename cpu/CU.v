44d8cceace7c863c21559efc1c6650542d3906c1
Sebastian Oliver <sebtheoliver@gmail.com>
2021-10-30 17:27:52 +0200

    Added skeleton provided for the simple CPU.

---
+++ cpu/CU.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/17/2021 06:12:17 PM
+// Design Name: 
+// Module Name: CU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CU(
+    input instruction,
+    input clk,
+    input PC,
+    input rst,
+    output [3:0] opcode
+    );
+endmodule
