5b832e200b02d6650e724f7aeb96a81d556a37fd
Sebastian Oliver <sebtheoliver@gmail.com>
2021-11-05 21:07:18 +0200

    Added a tcp send method that sends the sensor data to the server, using the local ip.

--- Client/src/main.py
+++ Client/src/main.py
@@ -1,4 +1,3 @@
-from flask import Flask
 import busio
 import time
 import digitalio
@@ -8,12 +7,10 @@ from adafruit_mcp3xxx.analog_in import AnalogIn
 import threading
 import RPi.GPIO as GPIO
 import sys
-
-app = Flask(__name__)
+import socket
 
 global chan0, chan1, step
 
-@app.route('/')
 # Prints out the values read from the thermistor and LDR.
 def check_and_print(name):
     global chan1, step
@@ -37,7 +34,30 @@ def check_and_print(name):
             adc_light, adc_temp = get_new_vals()
             temp = get_temp(chan1.voltage)
             print_out(adc_temp, temp, adc_light, value)
+            send(adc_temp, temp, adc_light, value)
             start = time.time()
+
+# Sends sensor info to the server through tcp.
+def send(adc_temp, temp, adc_light, value):
+    
+    # Specifying the ip and port of the server.
+    ip = "156.155.137.65"
+    port = 5005
+
+    buffer = 1024
+    data = adc_temp + "-" + temp + "-" + adc_light + "-" + value
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, port))
+    s.send(data)
+    confirm = s.recv(buffer)
+    
+    # Checking whether the server received the message or not.
+    while ("Y" not in confirm):
+        print("Data not received. \nResending data...")
+        s.send(data)
+        confirm = s.recv(buffer)
+    print("Data received.")
 
 # Function returns the raw ADC values of thermistor and LDR.
 def get_new_vals():
@@ -61,10 +81,6 @@ def print_out(temp_v, temp, light_v, timeCount):
     print(f"{timeCount}\t\t{temp_v}      \t\t{temp} C\t\t{light_v}")
 
 if (__name__=="__main__"):
-
-    app.run(host='0.0.0.0', port=80)
-
-    #global chan0, chan1, step
 
     # create the spi bus
     spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
